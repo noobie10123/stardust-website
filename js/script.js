6d3bf246bfda4e44d97ec6ccd4b1346dd2fec7fa
Ananya <ananya@Ananyas-MacBook-Air.local>
2020-09-08 19:56:08 +0530

    updated

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ var  mns = "sticky-top";
 var  hdr = $('#header').height();
 
 $(window).scroll(function() {
-  if( $(this).scrollTop() > (hdr+80) ) {
+  if( $(this).scrollTop() > (hdr+0) ) {
     mn.addClass(mns);
     mn.css("position","sticky");
   } else {
